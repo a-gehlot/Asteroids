df22613805f2b5e33640379e32c278a01cbaf7d2
AGehlot <andrew.gehlot@gmail.com>
2021-07-09 20:31:56 -0400

    Added applicable src file names

---
+++ src/moving_object.js
@@ -0,0 +1,5 @@
+// Base class for anything that moves.
+// Most important methods are 
+// MovingObject.prototype.move, 
+// MovingObject.prototype.draw(ctx), 
+// MovingObject.prototype.isCollidedWith(otherMovingObject)
