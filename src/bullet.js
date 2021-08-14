77eb8306200b57eadb1581b6efd2831c1882920a
AGehlot <andrew.gehlot@gmail.com>
2021-08-14 19:59:59 -0400

    Bullet class created

--- src/bullet.js
+++ src/bullet.js
@@ -1,1 +1,15 @@
 // Kill spacerocks with this. Also a MovingObject subclass.
+
+const MovingObject = require("./moving_object");
+const Util = require("./utils");
+
+function Bullet (optionsObject) {
+    optionsObject.RADIUS = Bullet.RADIUS;
+    MovingObject.call(this, optionsObject)
+}
+
+Bullet.RADIUS = 4;
+
+Util.inherits(Bullet, MovingObject);
+
+module.exports = Bullet;
